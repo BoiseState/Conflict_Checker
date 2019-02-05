9d5bf918b8536dd91029a5d21a8ed7b3ccc2c86a
JordanPaoletti <paoletti.jordan@yahoo.com>
2019-02-04 23:36:40 -0700

    #16 Added functions for reading and writing class constraints to file

---
+++ src/main/kotlin/bsu/cc/constraints/Models.kt
@@ -0,0 +1,23 @@
+package bsu.cc.constraints
+
+enum class ConstraintPriority(val prettyString: String) {
+    PRIORITY("priority"),
+    NON_PRIORITY("non-priority")
+}
+
+fun createPriorityFrom(str: String): ConstraintPriority {
+    return when (str) {
+        ConstraintPriority.PRIORITY.prettyString ->
+            ConstraintPriority.PRIORITY
+        ConstraintPriority.NON_PRIORITY.prettyString ->
+            ConstraintPriority.NON_PRIORITY
+        else -> throw IllegalArgumentException("Unknown priority: ${str}")
+    }
+}
+
+
+data class ClassConstraint(
+        val id: Int,
+        val priority: ConstraintPriority,
+        val classes: List<String>
+)
