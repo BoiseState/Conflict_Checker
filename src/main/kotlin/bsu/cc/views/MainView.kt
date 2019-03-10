3d5126b2c9e1fba11810e3eb869c4bc9a6954008
kolthahn97 <koltonhahn@u.boisestate.edu>
2019-03-09 18:00:00 -0700

    Adding a tableView to display conflicts.

--- src/main/kotlin/bsu/cc/views/MainView.kt
+++ src/main/kotlin/bsu/cc/views/MainView.kt
@@ -2,10 +2,16 @@ package bsu.cc.views
 
 import bsu.cc.Styles
 import bsu.cc.parser.DemoParser
+import javafx.beans.property.SimpleIntegerProperty
+import javafx.beans.property.SimpleObjectProperty
+import javafx.beans.property.SimpleStringProperty
 import javafx.scene.control.Alert.AlertType.INFORMATION
 import javafx.scene.control.TextField
 import javafx.scene.control.SelectionMode
+import javafx.scene.paint.Color
 import tornadofx.*
+import java.time.LocalDate
+import java.time.Period
 
 class MainView : View("Conflict Checker") {
     var fileNameField: TextField by singleAssign()
@@ -54,11 +60,21 @@ class MainView : View("Conflict Checker") {
                         DemoParser.producerTest(fileNameField.text)
                     }
                 }
-                val greekLetters = listOf("Alpha","Beta",
-                        "Gamma","Delta","Epsilon").observable()
-                listview(greekLetters) {
-                    selectionModel.selectionMode = SelectionMode.SINGLE
+
+                val persons = listOf(
+                        Person(1,"Samantha Stuart",LocalDate.of(1981,12,4)),
+                        Person(2,"Tom Marks",LocalDate.of(2001,1,23)),
+                        Person(3,"Stuart Gills",LocalDate.of(1989,5,23)),
+                        Person(3,"Nicole Williams",LocalDate.of(1998,8,11))
+                ).observable()
+
+                tableview(persons) {
+                    column("ID", Person::idProperty)
+                    column("Name", Person::nameProperty)
+                    column("Birthday", Person::birthdayProperty)
+                    column("Age", Person::ageProperty)
                 }
+
                 button("Highlight Test") {
                     setOnAction{
                         DemoParser.highlightTest(fileNameField.text)
@@ -74,4 +90,18 @@ class MainView : View("Conflict Checker") {
             }
         }
     }
+}
+
+class Person(id: Int, name: String, birthday: LocalDate) {
+    val idProperty = SimpleIntegerProperty(id)
+    var id by idProperty
+
+    val nameProperty = SimpleStringProperty(name)
+    var name by nameProperty
+
+    val birthdayProperty = SimpleObjectProperty(birthday)
+    var birthday by birthdayProperty
+
+    // Make age an observable value as well
+    val ageProperty = birthdayProperty.objectBinding { Period.between(it, LocalDate.now()).years }
 }
